76dfd37eca3d422ef4d6d35b2d9f772f284ee5b1
Chetan-7065 <131577082+Chetan-7065@users.noreply.github.com>
2025-11-06 00:37:46 +0530

    Update db.connect.js

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,7 +4,11 @@ require("dotenv").config()
 const mongoUri = process.env.MONGODB
 
 const initializeDatabase = async () => {
- await mongoose.connect(mongoUri).then(() => {
+ await mongoose.connect(mongoUri, {
+    maxPoolSize: 20,
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 45000
+  }).then(() => {
     console.log("Connected Successfully")
   }).catch((error) => {
      console.log("Connection failed", error)
