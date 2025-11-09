589e20abd7ee2a2f5337fe2092e6baa1c5b13505
Chetan-7065 <131577082+Chetan-7065@users.noreply.github.com>
2025-11-09 18:27:59 +0530

    Update db.connect.js

--- db/db.connect.js
+++ db/db.connect.js
@@ -7,7 +7,10 @@ const initializeDatabase = async () => {
  await mongoose.connect(mongoUri, {
     maxPoolSize: 20,
     serverSelectionTimeoutMS: 30000,
-    socketTimeoutMS: 45000
+    socketTimeoutMS: 45000,
+    connectTimeoutMS: 30000, 
+      retryWrites: true,    
+      retryReads: true
   }).then(() => {
     console.log("Connected Successfully")
   }).catch((error) => {
